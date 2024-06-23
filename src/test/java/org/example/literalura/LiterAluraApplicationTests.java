ab76cc41f8fa96036fc244730a1686b46ccab751
giordy henao <henaogiordy@gmail.com>
2024-06-23 11:46:37 -0500

    first commit

---
+++ src/test/java/org/example/literalura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
